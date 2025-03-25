34befc9c68ef5f8027fddc2ec16af37fda4c3a4f
rvishakhs <rvishakhs@gmail.com>
2025-03-25 11:10:20 +0000

    for the first time I'm getting response

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 from flask import Flask, render_template, jsonify, request
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_groq.chat_models import ChatGroq
 from src.prompt import * 
 import chromadb
 import os
 
-from store_index import get_query_results
+os.environ["GROQ_API_KEY"] = "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"
 
 # Initialising flask 
 app = Flask(__name__)
 
 # Retriving Chroma db collection and chain them into LLM 
-query_reslts = get_query_results()
+
+embeddings = HuggingFaceEmbeddings()
+
+persistant_db = 'doc_db'
+
+vectordb = Chroma(
+    embedding_function=embeddings,
+    persist_directory=persistant_db,
+)
+
+retriever = vectordb.as_retriever()
 
 Prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
 
 chain_type_kwargs = {"prompt" : Prompt}
 
+llm = ChatGroq(
+    model="deepseek-r1-distill-llama-70b",
+    temperature=0.5,
 
-qa = RetrievalQA.from_chain_type(
-    llm="gpt2",
-    chain_type="stuff",
-    retriever=get_query_results(),
-    return_source_documents = True,
-    chain_type_kwargs=chain_type_kwargs
 )
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=retriever,
+    chain_type="stuff",
+    return_source_documents=True
+)
+
 
 # App Routing 
 @app.route('/')
@@ -36,7 +54,7 @@ def chat():
     msg = request.form['msg']
     input = msg
     print(input)
-    result=qa({"query":input})
+    result=qa_chain({"query":input})
     return str(result["result"])
 
 
--- store_index.py
+++ store_index.py
@@ -1,5 +1,7 @@
 import chromadb
 import uuid
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
 from src.helper import load_pdf, text_split, RecursiveCharacterTextSplitter
 
 
@@ -9,26 +11,44 @@ extracted_data = load_pdf('/Users/visakh/Desktop/Gen_AI/medical_chat_bot/Data/')
 
 text_chunks = text_split(extracted_data)
 
-# Creating chromadb client
-chroma_client = chromadb.Client()
+# Load the embedding model 
+embeddings = HuggingFaceEmbeddings()
 
-collection = chroma_client.create_collection(name="medical_chat_bot")
+# Create a local db for chroma
+persistant_db = 'doc_db'
 
-sample_chunks = text_chunks[:int(len(text_chunks) * 0.01)]
-documents = [chunk.page_content for chunk in sample_chunks]
-ids = [str(uuid.uuid4()) for _ in documents]
+# Creating sample files for testing purpose 
+import random
+sampled_chunks = random.sample(text_chunks, max(1, int(len(text_chunks) * 0.005)))
 
-collection.add(
-    documents=documents,
-    ids=ids
+vectordb = Chroma.from_documents(
+    documents=sampled_chunks,
+    embedding=embeddings,
+    persist_directory=persistant_db,
 )
 
-def get_query_results(question):
-    results = collection.query(
-        query_texts=[question],
-        n_results=5,
-        where={"metadata_field": "is_equal_to_this"},
-        where_document={"$contains":"search_string"}
-    )
-    
-    return results
+
+
+# # Creating chromadb client
+# chroma_client = chromadb.Client()
+
+# collection = chroma_client.create_collection(name="medical_chat_bot")
+
+# sample_chunks = text_chunks[:int(len(text_chunks) * 0.01)]
+# documents = [chunk.page_content for chunk in sample_chunks]
+# ids = [str(uuid.uuid4()) for _ in documents]
+
+# collection.add(
+#     documents=documents,
+#     ids=ids
+# )
+
+# def get_query_results(question):
+#     results = collection.query(
+#         query_texts=[question],
+#         n_results=5,
+#         where={"metadata_field": "is_equal_to_this"},
+#         where_document={"$contains":"search_string"}
+#     )
+
+#     return results
